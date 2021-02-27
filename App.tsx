7d54d6db51a44eaa79a2641d5d5f8fee51c3a358
RennanD <rennan.douglas@objetivopi.com.br>
2021-02-27 11:31:13 -0300

    feat: Configure style guides

--- App.tsx
+++ App.tsx
@@ -1,15 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +8,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
