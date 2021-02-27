6cbd75ebb32c8118916241db46d8ca05b926ca2a
RennanD <rennan.douglas@objetivopi.com.br>
2021-02-27 11:51:54 -0300

    feat: Configure project structure

---
+++ src/components/ExperiencBar/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 0 20px;
+  background: #f2f2f2;
+`;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import { ExperienceBar } from '../../components/ExperiencBar';
+
+export function Home(): JSX.Element {
+  return (
+    <View>
+      <ExperienceBar />
+    </View>
+  );
+}
